39b2e81c15a51061bb281911b4d2c85518f4177c
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2022-06-19 14:11:34 +0100

    slice type

--- chapter_4/ownership/src/main.rs
+++ chapter_4/ownership/src/main.rs
@@ -89,6 +89,15 @@ fn main() {
 
     println!("The length of '{}' is {}.", s2, len);
 
+    // A reference guarentees a valid value of a particular type
+    
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+
 
     // REFERENCES AND BORROWING 
     // a reference is guaranteed to point to a valid value of a particular type 
